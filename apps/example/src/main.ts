f130fb577d0238371783b9456de77a774062c6ba
Gryphonn <gryphonn@yandex.ru>
2025-12-27 12:38:12 +0300

    something

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -1,7 +1,9 @@
 import { Render } from "@tldraw/render";
 import "./style.css";
+import { Editor } from "@tldraw/editor";
 
 const render = new Render();
+const editor = new Editor();
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
  Hey
