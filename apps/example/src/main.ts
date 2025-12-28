18966e3e5b7a3d69e0c60efa28c03c92c479409e
Gryphonn <gryphonn@yandex.ru>
2025-12-28 08:25:20 +0300

    some types shit

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -1,9 +1,10 @@
-import { Render } from "@tldraw/render";
+import { HandTool, Render } from "@tldraw/render";
 import "./style.css";
 import { Editor } from "@tldraw/editor";
 
 const render = new Render();
-const editor = new Editor();
+
+const editor = new Editor({ tools: [HandTool] });
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
  Hey
